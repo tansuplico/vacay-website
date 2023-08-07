4ffb1dd754996a47421fea128d4a294cc9a85eea
Tristan Suplico <tsuplico772@gmail.com>
2023-08-07 23:48:19 +0800

    banner form fixed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -22,7 +22,7 @@ const Banner = () => {
           <span> Book Hotels and Explore the world at the lowest price </span>
         </div>
         <div className="w-[85%] h-[370px] form:h-[200px] lg:h-[120px] px-5 lg:px-8 py-[4rem] form:py-[20px] backdrop-blur-sm bg-[#F0F8FF]/80 rounded-[30px] absolute bottom-[-15rem] form:bottom-[-8rem] lg:bottom-[-3rem] left-[8%] flex justify-center items-center ">
-          <form className="w-full py-5 rounded-[30px] flex flex-col justify-center items-center gap-5 form:grid form:grid-cols-7 ">
+          <form className="w-full py-5 rounded-[30px] grid  justify-center items-center gap-5  form:grid-cols-7 ">
             <div className="col-span-3 lg:flex gap-5  ">
               <div className="w-full h-[48px] mb-[10px] ">
                 <div>
@@ -87,7 +87,7 @@ const Banner = () => {
               to="/hotels"
               state={{ location: bannerLocation, guests: bannerGuests }}
             >
-              <div className="w-[45px] h-[45px] lg:ml-[30px] rounded-3xl flex justify-center items-center cursor-pointer hover:bg-slate-100 transition cols-span-1">
+              <div className=" w-[45px] h-[45px] lg:ml-[30px] rounded-3xl flex justify-center items-center cursor-pointer hover:bg-slate-100 transition col-span-3">
                 <img className="w-[30px] h-[30px]" src={search} alt="search" />
               </div>
             </Link>
